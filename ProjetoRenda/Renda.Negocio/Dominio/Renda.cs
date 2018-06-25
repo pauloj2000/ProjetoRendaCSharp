63fc84398e1a93b03f84aac14a16e33e287bb519
Paulo Junio Sales Rodrigues <paulojsalles@gmail.com>
2018-06-25 13:40:17 -0300

    Implantação do Entity Framework

---
+++ ProjetoRenda/Renda.Negocio/Dominio/Renda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renda.Negocio.Dominio
+{
+    public class Renda
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public double RendaMensal { get; set; }
+        public DateTime MesReferencia { get; set; }
+            
+    }
+}
---
+++ ProjetoRenda/Renda.Persistencia.EF/UsuarioDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Renda.Negocio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renda.Persistencia.EF
+{
+    public class UsuarioDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+    }
+}
